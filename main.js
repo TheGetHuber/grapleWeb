03f691daebf6d661cf53712fd4040988b8d63805
TheGetHuber <reidolf.danila@gmail.com>
2023-01-01 12:39:03 +0300

    toolbar stuff

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let toolbar = document.body
 
 window.onload = function(){
     toolbar = document.querySelector(".toolbar")
-    document.body.style.background = "linear-gradient("+ Math.sin(window.scrollY * 100) +"deg, #a02a7c, #30b4b4)"
+    
 }
 
 document.addEventListener("scroll", function(){
-    document.body.style.background = "linear-gradient("+ Math.sin(window.scrollY * 100) +"deg, #a02a7c, #30b4b4)"
+    
 })
